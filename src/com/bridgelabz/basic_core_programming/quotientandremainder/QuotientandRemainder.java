decb4ce09e55faea4be246fc57be8f0f0f5091f1
mayavathi <mayachandre27@gmail.com>
2021-06-23 14:01:56 -0400

    quotient and remainder

---
+++ src/com/bridgelabz/basic_core_programming/quotientandremainder/QuotientandRemainder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.basic_core_programming.quotientandremainder;
+
+public class QuotientandRemainder {
+	public static void main(String[] args)
+    {
+  
+        int dividend = 556, divisor = 9;
+  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
+

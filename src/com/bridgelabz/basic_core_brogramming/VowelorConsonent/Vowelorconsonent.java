2f382a339cb1a8d071f97271f529de194627e4b6
MayaChandre <84507177+MayaChandre@users.noreply.github.com>
2021-06-23 22:28:34 -0400

    Update Vowelorconsonent.java

--- src/com/bridgelabz/basic_core_brogramming/VowelorConsonent/Vowelorconsonent.java
+++ src/com/bridgelabz/basic_core_brogramming/VowelorConsonent/Vowelorconsonent.java
@@ -4,18 +4,12 @@ public class Vowelorconsonent {
 
 	public static void main(String[] args) {
 
-        char ch = 'z';
+        char ch = 'i';
 
-        switch (ch) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                System.out.println(ch + " is vowel");
-                break;
-            default:
-                System.out.println(ch + " is consonant");
-        }
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
     }
 }

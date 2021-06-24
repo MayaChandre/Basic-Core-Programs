402c6cc8a503ec821fd51be957ece942efab4069
mayavathi <mayachandre27@gmail.com>
2021-06-23 22:16:40 -0400

    Vowel or Consonent

--- src/com/bridgelabz/basic_core_brogramming/VowelorConsonent/Vowelorconsonent.java
+++ src/com/bridgelabz/basic_core_brogramming/VowelorConsonent/Vowelorconsonent.java
@@ -2,4 +2,20 @@ package com.bridgelabz.basic_core_brogramming.VowelorConsonent;
 
 public class Vowelorconsonent {
 
+	public static void main(String[] args) {
+
+        char ch = 'z';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
 }
